614f371dc180983a9b1cc80ba8a06409b80a7b68
nonk123 <Sergeyii321@gmail.com>
2020-04-30 20:46:11 +0300

    Better form parsing, add basic arithmetic functions

--- coop_excel/lisp.py
+++ coop_excel/lisp.py
@@ -8,10 +8,30 @@ def evaluate(table, expression):
     if not form:
         return cast(expression)
 
-    form = re.findall(r"\(.*?\)|[^\s]+", form.group("form"))
+    expression = form.group("form")
+
+    form = [""]
+
+    depth = 0
+
+    for c in expression:
+        if depth == 0 and c == " ":
+            form.append("")
+            continue
+
+        if c == "(":
+            depth += 1
+
+        if c == ")":
+            depth -= 1
+
+        form[-1] += c
+
+    if not form:
+        return None
 
     fun = form[0]
-    args = form[1:] if len(form) > 1 else []
+    args = form[1:]
 
     if fun in functions:
         try:
@@ -23,6 +43,9 @@ def evaluate(table, expression):
     return None
 
 def cast(expr):
+    if callable(expr):
+        return expr
+
     expr = str(expr)
 
     string_match = re.fullmatch(r'(?<=\").*(?=\")', expr)
@@ -31,10 +54,7 @@ def cast(expr):
         return string_match.group(0)
 
     try:
-        if float(expr) == int(expr):
-            return int(expr)
-        else:
-            return float(expr)
+        return float(expr)
     except:
         return None
 
@@ -47,14 +67,34 @@ def lisp_fn(name=None):
 
 @lisp_fn()
 def at(table, row, col):
-    if isinstance(row, int) and isinstance(col, int):
-        return table.get(row, col).value
+    return table.get(int(row), int(col)).value
+
+def reduce(fun, *args):
+    if not args:
+        return None
+
+    x = args[0]
+
+    for y in args[1:]:
+        x = fun(x, y)
+
+    return x
 
 @lisp_fn("+")
-def plus(table, *args):
-    s = 0
+def add(table, *args):
+    return reduce(lambda x, y: x + y, *args)
 
-    for x in args:
-        s += x
+@lisp_fn("-")
+def subtract(table, *args):
+    if len(args) == 1:
+        return -args[0]
+    else:
+        return reduce(lambda x, y: x - y, *args)
 
-    return s
+@lisp_fn("*")
+def multiply(table, *args):
+    return reduce(lambda x, y: x * y, *args)
+
+@lisp_fn("/")
+def divide(table, *args):
+    return reduce(lambda x, y: x / y, *args)
